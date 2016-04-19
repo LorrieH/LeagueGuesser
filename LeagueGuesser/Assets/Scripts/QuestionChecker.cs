2fc39846f8e7c64df9c2c47be3c05615c259d284
Jeffreeyy <17844@ma-web.nl>
2016-04-19 19:43:48 +0200

    Working question generator and checker

---
+++ LeagueGuesser/Assets/Scripts/QuestionChecker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuestionChecker : MonoBehaviour 
+{
+    private QuestionGenerator questionGenerator;
+    [SerializeField]private InputField inputAnswer;
+
+	void Start () 
+    {
+        questionGenerator = GetComponent<QuestionGenerator>();
+	}
+	
+    public void CheckQuestion()
+    {
+        if(inputAnswer.text == questionGenerator.currentQuestion.questionAnswers[0])
+        {
+            Debug.Log("AYY je hebt het goed man");
+            questionGenerator.NextQuestion();
+            inputAnswer.text = "";
+        }
+        else
+        {
+            Debug.Log("Never lucky man");
+        }
+    }
+
+}
---
+++ LeagueGuesser/Assets/Scripts/QuestionGenerator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuestionGenerator : MonoBehaviour {
+
+    public Question[] questions;
+    public Question currentQuestion;
+
+    [SerializeField]private Text questionText;
+    [SerializeField]private Image questionImage;
+
+    void Start()
+    {
+        NextQuestion();
+    }
+
+	public void NextQuestion()
+    {
+        currentQuestion = GenerateQuestion();
+        questionText.text = currentQuestion.questionString;
+        questionImage.sprite = currentQuestion.questionImage;
+	}
+
+
+
+    private Question GenerateQuestion()
+    {
+        int index = Random.Range(0,questions.Length);
+        return questions[index];
+    }
+
+}
