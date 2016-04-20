52a3fe23168ca5df28ea7776a02025ba18ef2c49
Lorenzo <18927@ma-web.nl>
2016-04-20 11:46:55 +0200

    savename

---
+++ LeagueGuesser/Assets/Scripts/CheckForFirstTime.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CheckForFirstTime : MonoBehaviour {
+
+    private SaveName saveName;
+    [SerializeField]private GameObject panel;
+	// Use this for initialization
+	void Start () {
+        saveName = GameObject.FindWithTag("DataObject").GetComponent<SaveName>();
+        CheckFirstTime();
+        Debug.Log(saveName.ShowPanel);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(saveName.ShowPanel);
+    }
+
+    void CheckFirstTime()
+    {
+       
+        if (saveName.ShowPanel == false)
+        {
+            
+            panel.SetActive(!panel.activeSelf);
+        }
+    }
+}
---
+++ LeagueGuesser/Assets/Scripts/SaveName.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.UI;
+
+public class SaveName : MonoBehaviour {
+
+    [SerializeField]private InputField nameInput;
+    [SerializeField]private bool showPanel;
+    public bool ShowPanel
+    {
+        get
+        {
+            return showPanel;
+        }
+        set
+        {
+            showPanel = value;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        GetName();
+	}
+
+    public void StoreName(string nickname)
+    {
+        nickname = nameInput.text;
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/NameData.elohell");
+        EnterName enterName = new EnterName();
+        enterName.name = nickname;
+        enterName.firstTime = false;
+
+        bf.Serialize(file, enterName);
+        file.Close();
+    }
+
+    public void GetName()
+    {
+        if(File.Exists(Application.persistentDataPath + "/NameData.elohell"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/NameData.elohell", FileMode.Open);
+
+            EnterName saveData = (EnterName)bf.Deserialize(file);
+            showPanel = saveData.firstTime;
+            Debug.Log("your nickname = " + saveData.name + "firstTime = " + showPanel);
+            file.Close();
+        }
+    }
+}
+
+[System.Serializable]
+public class EnterName
+{
+    public string name;
+    public bool firstTime;
+}
