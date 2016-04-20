8352b614472ff16bc4633e81efbfe51560d1db64
Lorenzo <18927@ma-web.nl>
2016-04-20 09:43:50 +0200

    fade in image

---
+++ LeagueGuesser/Assets/Scripts/FadeImage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeImage : MonoBehaviour {
+
+    private Image fadeImage;
+    private Color c;
+    private float alpha;
+
+	// Use this for initialization
+	void Start () {
+        fadeImage = GetComponent<Image>();
+
+        c = fadeImage.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(c.a < 1)
+        {
+            UpdateColor();
+        }
+	}
+
+    void UpdateColor()
+    {
+        alpha = alpha + 0.01f;
+        c.a = alpha;
+        fadeImage.color = c;
+    }
+}
